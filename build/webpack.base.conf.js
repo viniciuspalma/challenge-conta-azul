cdc56492a538a6040cd69c2ef693da83fc4897d0
viniciuspalma <pockvini@gmail.com>
2017-02-21 17:45:20 -0300

    create a file to store all sass variables

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,7 +19,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.scss'],
     modules: [
       resolve('src'),
       resolve('node_modules')
@@ -28,7 +28,8 @@ module.exports = {
       'vue$': 'vue/dist/vue.common.js',
       'src': resolve('src'),
       'assets': resolve('src/assets'),
-      'components': resolve('src/components')
+      'components': resolve('src/components'),
+      'styles': resolve('src/styles')
     }
   },
   module: {
