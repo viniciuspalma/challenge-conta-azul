660b9ee13121763d64d86870ea7b04d2e44e772b
viniciuspalma <pockvini@gmail.com>
2017-02-22 00:04:44 -0300

    add the component newcar in the routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'components/Home'
+import NewCar from 'components/cars/NewCar'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/cars/new',
+      name: 'NewCar',
+      component: NewCar
     }
   ]
 })
